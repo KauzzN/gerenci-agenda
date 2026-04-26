edbe703826b757218889118c418bd65a36b635eb
Kauzz <kaynnanjeckson@gmail.com>
2026-04-26 14:13:14 -0300

    fix

--- agendamento/forms.py
+++ agendamento/forms.py
@@ -1,10 +1,29 @@
 from django import forms
+from django.utils import timezone
 from .models import Agendamento
+
+
 
 class AgendamentoForm(forms.ModelForm):
     class Meta:
         model = Agendamento
         fields = ['nome', 'horario']
         widgets = {
-            'horario': forms.DateTimeInput(attrs={'type': 'datetime-local'})
+            'horario': forms.DateTimeInput(attrs={
+                'type': 'datetime-local',
+                'class': 'form-control',
+                'step': '1800'
+            })
         }
+
+    def clean_horario(self):
+        horario = self.cleaned_data.get('horario')
+
+        if horario:
+            if timezone.is_naive(horario):
+                horario = timezone.make_aware(horario, timezone.get_current_timezone())
+
+            if horario < timezone.localtime():
+                raise forms.ValidationError('Não pode agendar no passado.')
+
+        return horario
--- agendamento/urls.py
+++ agendamento/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.lista_agendamentos, name='lista'),
     path('novo/', views.criar_agendamento, name='novo'),
-    path('editar/,int:id>/', views.editar_agendamento, name='editar'),
+    path('editar/<int:id>/', views.editar_agendamento, name='editar'),
     path('excluir/<int:id>/', views.excluir_agendamento, name='excluir'),
     path('atendido/<int:id>/', views.marcar_atendido, name='atendido')
 ]
