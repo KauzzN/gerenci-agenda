e16cd06bca85e3cf62e3b7e58a985690a1abdb5c
Kauzz <kaynnanjeckson@gmail.com>
2026-04-28 18:57:27 -0300

    add

--- core/settings.py
+++ core/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = 'django-insecure-4mw9)nh#%g^^(&z8f+xma8iylp&2nj4+-1ilobk^gsq&lve6=@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.ngrok-free.dev",
+]
 
 # Application definition
 
