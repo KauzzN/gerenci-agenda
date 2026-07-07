b0ea4b46ba4823ab56fd0f9142df3116ad2ae91b
Kauzz <kaynnanjeckson@gmail.com>
2026-07-07 19:26:09 -0300

    consertando conflitos de rota

--- agendamento/urls.py
+++ agendamento/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('/agendar/update/<int:id_agend>', views.update_agendamentos, name='update'),
     path('/agendar/delete/<int:id_agend>', views.delete_agendamento, name='delete'),
     path('/agendar/status/<int:id_agend>', views.atualizar_status, name='status'),
-    path('agendar/dashboard',  views.dashboard_view, name='dashboard'),
+    path('/agendar/dashboard',  views.dashboard_view, name='dashboard'),
     path('/agendar/historico', views.historico_view, name='historico')
 ]
